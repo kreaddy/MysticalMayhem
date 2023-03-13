96eb2dd8a40175a89c8fd65c0ce85cbd3fed9a38
Starion <oli.pasc@laposte.net>
2023-03-13 23:29:12 +0100

    Fixed Sure Casting and added scroll.

---
+++ MysticalMayhem/Mechanics/ContextActionRemoveSelfNextTime.cs
@@ -0,0 +1,37 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.ElementsSystem;
+using Kingmaker.UnitLogic.Abilities.Components.AreaEffects;
+using Kingmaker.UnitLogic.Buffs;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+
+namespace MysticalMayhem.Mechanics
+{
+    [TypeId("8f870b13-dacd-40da-8c65-3ecba11f718f")]
+    public class ContextActionRemoveSelfNextTime : ContextAction
+    {
+        private int _cdown = 1;
+
+        public override string GetCaption()
+        {
+            return "Remove self but not after X";
+        }
+
+        public override void RunAction()
+        {
+            if (_cdown > 0) { _cdown -= 1; return; }
+            Buff.Data data = ContextData<Buff.Data>.Current;
+            if (data != null)
+            {
+                data.Buff.Remove();
+                return;
+            }
+            AreaEffectContextData areaEffectContextData = ContextData<AreaEffectContextData>.Current;
+            if (areaEffectContextData != null)
+            {
+                areaEffectContextData.Entity.ForceEnd();
+                return;
+            }
+        }
+    }
+}
